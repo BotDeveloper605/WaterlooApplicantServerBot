7b29e11e638d692b4dc9ce08925b0e54e996e7d5
Bot Developer <ccntdeveloper@gmail.com>
2022-01-29 17:52:36 -0500

    Refactor abbreviations

--- data_model/admissions_data.py
+++ data_model/admissions_data.py
@@ -4,31 +4,7 @@ import pandas
 SummaryData = namedtuple('SummaryData', ['low', 'high', 'num_applicants'])
 
 class AdmissionsData():
-    # Allows for easier common queries against admissions data
-
-    # Allow for common short-hands
-    __program_abbreviations = {
-        'AFM': 'Accounting and Financial Management',
-        'FARM': 'Financial Analysis Risk Management',
-        'Math': 'Mathematics',
-        'Math/BBA': 'Mathematics and Business Administration',
-        'BMath/BBA': 'Mathematics and Business Administration',
-        'CS': 'Computer Science',
-        'CS/BBA': 'Computer Science and Business Administration',
-        'CFM': 'Computing and Financial Management',
-        'ARBUS': 'Arts and Business',
-        'ENBUS': 'Environment and Business',
-        'CE': 'Computer Engineering',
-        'EE': 'Electrical Engineering',
-        'SE': 'Software Engineering',
-        'SYDE': 'Systems Design Engineering',
-        'Chem Eng': 'Chemical Engineering',
-        'Mech Eng': 'Mechanical Engineering',
-        'Mechatronics': 'Mechatronics Engineering',
-        'Nano': 'Nanotechnology Engineering',
-        'Math/CPA': 'Mathematics and Chartered Professional Accountancy',
-        'Biotech/CPA': 'Biotechnology and Chartered Professional Accountancy',
-    }
+    # AdmissionsData Allows for easier common queries against admissions data
 
     @classmethod
     def data_years_available(cls):
@@ -37,6 +13,7 @@ class AdmissionsData():
     # TODO: Cache creation / singleton this?
     # TODO: protect against bad queries?
     def __init__(self, year: int):
+        # Load program data
         self.__data = pandas.read_csv("../data/waterloo_%d.csv" % year)
 
         self.__headers = ['Status', 'Program', 'Co-op', 'Grade Percentage', 'Date Accepted', 'Type', 'Other']
@@ -44,10 +21,18 @@ class AdmissionsData():
         # Keep only relevant columns
         self.__data = self.__data[self.__headers]
 
+        # == Load program abbreviations == 
+        self.__abbreviations = pandas.read_csv('abbreviations.csv')
+
+    # Convert from a short version to the full names
+    def __translate_program_name(self, program_name):
+        if (program_name in self.__abbreviations['Short'].unique()):
+            program_name = self.__abbreviations.loc[self.__abbreviations['Short'] == program_name, 'Long'].iloc[0]
+        return program_name
 
     # Check if we have data on a given program
     def program_data_exists(self, program_name: str) -> bool:
-        return self.__program_abbreviations.get(program_name, program_name) in self.__data['Program'].unique()
+        return self.__translate_program_name(program_name) in self.__data['Program'].unique()
 
 
     def __drop_invalid(self, df: pandas.DataFrame) -> pandas.DataFrame:
@@ -65,7 +50,7 @@ class AdmissionsData():
     # Return available summary information for a given program
     def get_program_data(self, program_name: str, *, status: str = None, applicant_type: str = None) -> SummaryData:
         # TODO: Add support for filtering based on other attributes?
-        program_name = self.__program_abbreviations.get(program_name, program_name)
+        program_name = self.__translate_program_name(program_name)
 
         data_slice = self.__data[self.__data['Program'] == program_name]
         data_slice = self.__drop_invalid(data_slice)
@@ -81,11 +66,8 @@ class AdmissionsData():
                 data_slice['Grade Percentage'].max(),
                 len(data_slice['Grade Percentage']))
 
-    # For testing
-    def get_data(self):
-        return self.__data
-
 
 if __name__ == "__main__":
     admissions_data = AdmissionsData(2021)
     cs_data = admissions_data.get_program_data('Computer Science')
+    cs_data2 = admissions_data.get_program_data('CS')
