df47fd9bd69166519082870c0b82dda455680873
Bot Developer <ccntdeveloper@gmail.com>
2022-01-29 23:09:52 -0500

    Basic argument passing supported

--- discord_cogs/admissions_cog.py
+++ discord_cogs/admissions_cog.py
@@ -6,8 +6,8 @@
 # __import__('IPython').embed()
 # ==================================================================
 
+from typing import Type, Dict, Callable
 from discord.ext import commands
-import discord
 
 from data_model.admissions_data import AdmissionsData, SummaryData
 
@@ -18,23 +18,47 @@ class AdmissionsCog(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Admissions Officer Cog has been loaded into {name} and is on the case!'.format(name = self.bot.user.name))
+        print('Admissions Officer has been loaded into {name} and is on the case!'.format(name = self.bot.user.name))
         print('=======================================')
 
 
     # Teach people how to use ratemychances command
-    def __usage_message_ratemychances(self):
+    def __error_usage_message_ratemychances(self):
         usage = """
             Please use the command in the following format:
             
-            !ratemychances program=PROGRAM average=AVERAGE [ type=TYPE ]
+            !ratemychances program=PROGRAM_CODE average=AVERAGE [ type=TYPE ]
 
             where:
+                - PROGRAM_CODE is the code from OUAC (only Waterloo program codes currently supported)
+                - AVERAGE is your *percentage* based average (please convert before usings)
                 - (optional) TYPE is one of [101, 105, 105D, 105F]
         """
 
         return usage
 
+    def parse_args(self, args_list: list) -> dict:
+        return dict(map(lambda x: x.split("="), args_list))
+
+    # Double check the passed in arguments conform to what's expected
+    # SIDE EFFECT:
+    #   - This will properly cast as many arguments of arg_dict as possible
+    #       (your strings will be gone.)
+    async def check_required_args(self, ctx, arg_dict: Dict[str, str], required_args: Dict[str, Type], error_message_generatror: Callable) -> bool:
+    
+        for req_arg in required_args:
+            if (req_arg not in arg_dict):
+                await ctx.send('{req_field} was not specified.'.format(req_field = req_arg))
+                await ctx.send(error_message_generatror())
+                return False
+            else:
+                try:
+                    arg_dict[req_arg] = required_args[req_arg](arg_dict[req_arg])
+                except Exception as e:
+                    await ctx.send('Could not cast {req_field} to be type {req_type}'.format(req_field = req_arg, req_type = required_args[req_arg].__name__))
+                    await ctx.send(error_message_generatror())
+                    return False
+        return True
 
     @commands.command(name='ratemychances')
     async def on_message(self, ctx: commands.context.Context, *args):
@@ -43,10 +67,26 @@ class AdmissionsCog(commands.Cog):
         if (ctx.author.id == self.bot.user.id):
             return
 
-        if (len(args) == 0):
-            await ctx.send(self.__usage_message_ratemychances())
+        try:
+            req_args = {'program': str, 'average': float}
+            arg_dict = self.parse_args(args)
+
+            # A radioactively toxic line of code, but damn, it felt good to write 
+            if (not await self.check_required_args(ctx, arg_dict, req_args, self.__error_usage_message_ratemychances)):
+                # We ran into an issue that we already caught.. don't worry about it any more.
+                return
+            
+            # ======= After this line, we should be guaranteed required arguments & matching types =====
+
+        except Exception as e:
+            await ctx.send('Error processing request')
+
+            if (len(e.args) > 0):
+                await ctx.send("Specificaly... {error}".format(error = e.args[0]))
+
+            await ctx.send(self.__error_usage_message_ratemychances())
             return
 
-        print(args)
+
         
         await ctx.send('no')
